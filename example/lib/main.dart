f6e442fd4b18ef4325e40af5396e0e2572868874
Alexander "PapaTutuWawa <papatutuwawa@polynom.me>
2023-07-21 17:31:43 +0200

    fix: Fix example code

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -101,11 +101,6 @@ class MyHomePage extends StatelessWidget {
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
